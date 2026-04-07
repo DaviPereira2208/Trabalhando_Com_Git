7c3c757b90a4d7e3c8de2dfc65f2b8c67f8d5719
DaviPereira2208 <davipereirasantos2208@gmail.com>
2026-04-07 15:10:25 -0300

    feat:adicione ex01

---
+++ ex01.js
@@ -0,0 +1,29 @@
+// const readline = require('readline-sync')
+
+
+// let nome = readline.question("Dige o seu nome: ")
+
+// console.log(`seu nome é: ${nome}`)
+
+
+//--------Exercico 1---------
+// Importa a biblioteca 
+// const readline = require("readline-sync")
+
+// // Solicita as informações do usuario
+// let nome = readline.question("Escreva o seu nome: ")
+// let curso = readline.question("Curso que está matriculado: ")
+// let idade = readline.questionInt("Informe a sua idade: ")
+
+// console.log("======== SISTEMA DE CADASTRO =========")
+// console.log(`Nome do Usuario: ${nome}`)
+// console.log(`Curso do usuario: ${curso}`)
+// console.log(`Idade do Usuario: ${idade}`)
+
+// //--------Exercicio 2----------
+// // Importar a Biblioteca
+// const readline = require("readline-sync")
+
+// //--------Solicita as informações do usuario------------
+// let 
+
