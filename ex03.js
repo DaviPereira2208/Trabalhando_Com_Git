4bc1eea14ef1da32073253182d93e93c05538f34
DaviPereira2208 <davipereirasantos2208@gmail.com>
2026-04-07 16:00:52 -0300

    feat:adicione ex03

---
+++ ex03.js
@@ -0,0 +1,14 @@
+//  // ------Importar a Biblioteca------
+//  const readline = require("readline-sync")
+
+//   //--------Solicita as informações do usuario------------
+// //   ---Solicita as informações do usuario------------
+  
+//   let nome = readline.question("Informe seu nome: ")
+//   let jogo = readline.question("Informe o seu jogo favorito: ")
+//   let pontuação = readline.questionFloat("Informe a sua pontuação atual: ")
+
+//   // console.log("======== Plataforma de Jogos =========")
+// console.log(`Usuario: ${nome}`)
+// console.log(`Jogo Favorito: ${jogo}`)
+// console.log(`Pontuação Atual: ${pontuação}`)
