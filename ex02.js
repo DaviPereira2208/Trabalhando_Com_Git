7fe394ed0459014ec7d2d0e7cbe3330612bdb725
DaviPereira2208 <davipereirasantos2208@gmail.com>
2026-04-07 16:00:32 -0300

    feat:adicione ex02

---
+++ ex02.js
@@ -0,0 +1,13 @@
+//  // ------Importar a Biblioteca------
+//  const readline = require("readline-sync")
+
+//  //--------Solicita as informações do usuario------------
+
+// let nome = readline.question("Informe seu nome: ")
+// let nomeDoPrato = readline.question("Informe o nome do prato: ")
+// let totalDoPrato = readline.questionFloat("Informe o valor do prato")
+
+// // console.log("======== Resummo Do Pedido =========")
+// console.log(`Cliente: ${nome}`)
+// console.log(`Prato Escolhido: ${nomeDoPrato}`)
+// console.log(`Valor do Prato: ${totalDoPrato}`)
